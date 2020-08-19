5440a01368482b38e53bd7401e3f9b6d8273de8c
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-19 15:57:57 +0300

    Lessons 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <Rating />
+      <Accordion />
     </div>
   );
+}
+
+const Rating = () => {
+  return (
+    <>
+      <Star />
+    </>
+  )
+
+}
+const Star = () => {
+  return (
+    <>
+      <div>star</div>
+    </>
+  )
+}
+
+const Accordion = () => {
+  return (
+    <>
+      <h3>Menu</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </>
+  )
 }
 
 export default App;

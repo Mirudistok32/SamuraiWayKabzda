5fdd0727cea3429870c77c7542e9d3ae3af8cefa
Mirudistok <mirudistok32@yandex.ru>
2020-08-29 18:38:09 +0300

    Create stories for UncontrolledOnOff component

---
+++ src/UncontrolledOnOff/UncontrolledOnOff.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Meta } from '@storybook/react/types-6-0';
+import { UncontrolledOnOff } from './UncontrolledOnOff';
+
+export default {
+  title: 'My Components/UncontrolledOnOff',
+  component: UncontrolledOnOff,
+  argTypes: {
+    
+  },
+} as Meta;
+
+export const Collapsed = () => <UncontrolledOnOff />;
